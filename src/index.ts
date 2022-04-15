edcb38d825addad99302ec30f4375ffe75b70db3
saltyaom <saltyaom@gmail.com>
2022-04-16 01:02:40 +0700

    :tada: feat: add prisma connect

--- src/index.ts
+++ src/index.ts
@@ -11,46 +11,34 @@ import cors from 'fastify-cors'
 import { resolve } from 'path'
 
 import { auth, base, favorite } from '@modules'
-import { run, mutateAuthHook } from '@services'
+import { mutateAuthHook } from '@services'
 
 const app = fastify()
 
-const main = () =>
-    app
-        .register(helmet)
-        .register(cookie)
-        .register(cors, {
-            origin: [/localhost:3000$/, /hifumin.app$/],
-            methods: [
-                'GET',
-                'HEAD',
-                'PUT',
-                'PATCH',
-                'POST',
-                'DELETE',
-                'OPTIONS'
-            ],
-            allowedHeaders: ['Content-Type'],
-            credentials: true,
-            preflight: true
-        })
-        .register(staticPlugin, {
-            root: resolve('./public')
-        })
-        .decorateRequest('userId', null)
-        .decorateRequest('auth', false)
-        .addHook('onRequest', mutateAuthHook)
-        .register(auth, {
-            prefix: '/auth'
-        })
-        .register(favorite, {
-            prefix: '/favorite'
-        })
-        .register(base)
-        .listen(process.env.PORT ?? 8080, '0.0.0.0', (error, address) => {
-            if (error) return console.error(error)
+app.register(helmet)
+    .register(cookie)
+    .register(cors, {
+        origin: [/localhost:3000$/, /hifumin.app$/],
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type'],
+        credentials: true,
+        preflight: true
+    })
+    .register(staticPlugin, {
+        root: resolve('./public')
+    })
+    .decorateRequest('userId', null)
+    .decorateRequest('auth', false)
+    .addHook('onRequest', mutateAuthHook)
+    .register(auth, {
+        prefix: '/auth'
+    })
+    .register(favorite, {
+        prefix: '/favorite'
+    })
+    .register(base)
+    .listen(process.env.PORT ?? 8080, '0.0.0.0', (error, address) => {
+        if (error) return console.error(error)
 
-            console.log(`Running at ${address}`)
-        })
-
-run(main)
+        console.log(`Running at ${address}`)
+    })
--- src/services/prisma/index.ts
+++ src/services/prisma/index.ts
@@ -2,4 +2,7 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+// Can't use top level await, but we only need to warm up from side-effect here
+prisma.$connect()
+
 export default prisma
